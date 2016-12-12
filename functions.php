559e405c0b1baac9a7502d4b84590d617d966d82
stoneliz <stoneliz@users.noreply.github.com>
2016-12-11 21:11:31 -0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,12 +1,10 @@
 <?php 
 add_action('widgets_init', 'my_sidebar');
-
 register_nav_menus(array(
 		"main-menu" => "Main website navigation",
 		"util-menu" => "Utilities for my website",
 		"footer-nav" => "Menu that appears in the footer"
 	)); 
-
 function my_sidebar(){
     /*Register the primary sidebar*/
     register_sidebar(
@@ -21,4 +19,17 @@ function my_sidebar(){
     )
     );
 }
+
+$seriously_upperfooter_sidebar = array(
+            'id'=>'upper-footer',
+            'name'=> 'Upper Footer',
+            'description' => 'Widgets placed here will go in the upper footer area',
+            'before_widget' => '<div class="widget">',
+            'after_widget' => '</div>',
+            'before_title' => '<h3 class="widget-title"><strong>',
+            'after_title' => '</strong></h3>'
+    );
+register_sidebar($seriously_upperfooter_sidebar);
+
+remove_action('wp_head', 'wp_generator');
 ?>
