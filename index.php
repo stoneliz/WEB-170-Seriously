3e45d1dd2dae0436c9bb8cd6bacde94236f4e2fd
stoneliz <stoneliz@users.noreply.github.com>
2016-12-12 16:41:20 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php get_header(); ?>
 <?php get_sidebar(); ?>
 <div id="content">
+
+<?php $seriously_slider_param = array(
+'cat' =>'5',
+'post_count'=> '5');
+
+$the_query = new WP_Query($seriously_slider_param);
+
+if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+<a href="<?php $seriously_slider_url = get_post_custom_values( 'url' ); echo $seriously_slider_url[0]; ?>" title="<?php the_title(); ?>"</a>
+
+<?php endwhile; endif;
+wp_reset_postdata(); ?>
     <!--the loop-->
+
     <?php if(have_posts()) : while(have_posts()) : the_post();?>
     <div class="entry">
         <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?>
