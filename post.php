269732abf8d15c3f1cf6f72b3e294f97e4d718c1
stoneliz <stoneliz@users.noreply.github.com>
2016-12-12 16:44:20 -0800

    Update post.php

--- post.php
+++ post.php
@@ -3,7 +3,12 @@
 <div id="content">
     <?php if (have_posts()) : while (have_posts()) : the_post();?>
     <div class="entry">
-        <h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+<blockquote>
+        <h2><?php the_title();?></h2>
+	<h3>By <?php the_author(); ?> </h3>
+	<p><strong><?php the_date(); ?> </strong></p>
+</blockquote>
+
         <div class="entry-body">
         <?php the_content();?>
         </div>
@@ -11,5 +16,7 @@
     <?php endwhile; else: ?>
     <p><?php ('Sorry, no posts matched your criteria.');?></p>
     <?php endif;?>
+
 </div>
+<?php dynamic_sidebar('Upper Footer'); ?>
 <?php get_footer();?>
